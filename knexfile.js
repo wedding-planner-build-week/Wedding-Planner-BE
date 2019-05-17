6c883ebe75033371cf304f26336fb5694ec78373
ntsum <natsukajiwaram@gmail.com>
2019-05-17 17:28:26 -0400

    added testing to knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,18 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
