e1611c9848b5c61474481b8d857bcb7f877cff53
ntsum <natsukajiwaram@gmail.com>
2019-05-18 18:50:04 -0400

    starting on routing for posts, running into some bugs, but going to call it a night:

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -9,10 +9,28 @@ exports.seed = function(knex, Promise) {
       location: "Boston, MA"
     },
     {
-      username: "dog",
+      username: "tina",
       password: bcrypt.hashSync("password", 14),
-      email: "ilovedogs@gmail.com",
+      email: "123@123.com",
+      location: "New York, NY"
+    },
+    {
+      username: "linda",
+      password: bcrypt.hashSync("password", 14),
+      email: "345@345.com",
       location: "San Diego, CA"
+    },
+    {
+      username: "sarah",
+      password: bcrypt.hashSync("password", 14),
+      email: "567@567.com",
+      location: "Austin, TX"
+    },
+    {
+      username: "karen",
+      password: bcrypt.hashSync("password", 14),
+      email: "789@789.com",
+      location: "Orlando, FL"
     }
   ]);
 };
